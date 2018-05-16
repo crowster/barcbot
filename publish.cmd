4b49a6dc5e36ad5cfb2a5baf30d07b789cc49d80
Jesusgmrtz <jesus.martinez@acftechnologies.com>
2018-05-16 13:22:10 -0500

    Add document files

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=barclaybankbot-Web-Deploy.pubxml -p:Password=Hd60nSbbrbzeJX0lQAbqfuZREEkhs3dlQNKkhb0qopScDqQrbZRcsleypd0l
+
