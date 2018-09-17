93d0af4bae856011d3a5a19084b20a3172a95e65
Jesusgmrtz <jesus.martinez@acftechnologies.com>
2018-09-17 16:20:01 -0500

    configuration in the global asax

--- Global.asax.cs
+++ Global.asax.cs
@@ -6,6 +6,9 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Azure;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Internals;
+using System.Security.Claims;
+using System.Web;
+using System.Reflection;
 
 namespace BarclayBankBot
 {
@@ -13,21 +16,21 @@ namespace BarclayBankBot
     {
         protected void Application_Start()
         {
-            var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
-            var key = ConfigurationManager.AppSettings["DocumentDbKey"];
-            var store = new DocumentDbBotDataStore(uri, key);
+          
             Conversation.UpdateContainer(
                builder =>
                {
+                   builder.RegisterModule(new AzureModule(Assembly.GetExecutingAssembly()));
+                   var uri = new Uri(ConfigurationManager.AppSettings["DocumentDbUrl"]);
+                   var key = ConfigurationManager.AppSettings["DocumentDbKey"];
+                   var store = new DocumentDbBotDataStore(uri, key);
+
                    builder.Register(c => store)
                        .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
                        .AsSelf()
                        .SingleInstance();
 
-                   builder.Register(c => new CachingBotDataStore(store, CachingBotDataStoreConsistencyPolicy.ETagBasedConsistency))
-                       .As<IBotDataStore<BotData>>()
-                       .AsSelf()
-                       .InstancePerLifetimeScope();
+
 
                });
             GlobalConfiguration.Configure(WebApiConfig.Register);
